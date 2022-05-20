a96cbeb68739164db472218fb4adcbe04e67826c
carozum <czumbiehlfaure@hotmail.fr>
2022-05-20 12:17:20 +0200

    ajout CSS

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- css links -->
+    <link rel="stylesheet" href="css/main.css">
+
+
+
+    <title>WOB Home Page</title>
+</head>
+
+<body>
+
+    <h2>Qu'est ce qui vous amène ici ?</h2>
+
+
+</body>
+
+</html>
